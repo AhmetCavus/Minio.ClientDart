f33fda1b43ff2bf36ca9021582de0475cb0cbe87
Ahmet Cavus <ahmet.cavus@aldi-nord.de>
2021-10-14 22:39:43 +0200

    Add cases for nested model tests

--- tests/integration/mini_client_test.dart
+++ tests/integration/mini_client_test.dart
@@ -7,10 +7,11 @@ import 'package:minio_client_dart/socket.events.dart';
 import 'package:test/test.dart';
 
 import '../category.dart';
+import '../todo.dart';
 
 void main() async {
   final minioClient = MinioClient.init("http://localhost:8080");
-  MinioCollection<Category> categories;
+  MinioCollection<Todo> todos;
   
   group("minio client integration tests", (){
     group("authentication", ()  {
@@ -41,28 +42,29 @@ void main() async {
 
       group("collection", () {
         test("get collection", () async {
-          categories = await minioClient.requestCollection<Category>((decoded) => Category(decoded));
-          expect(categories, isNotNull);
+          todos = await minioClient.requestCollection<Todo>((decoded) => Todo(decoded));
+          expect(todos, isNotNull);
         });
 
         var random = Random();
         var id = "id${random.nextInt(1000)}";
-        var newCategory = Category.Init(id, "name${random.nextInt(1000)}", "description${random.nextInt(1000)}", "https://lorempicsum.com/300");
+        var newCategory = Todo.Init(id, "name${random.nextInt(1000)}", "description${random.nextInt(1000)}", DateTime.now(), false, Category.Init("id", name, description, image));
         var addCategoryResult;
         
         test("post item to collection", () async {
-            addCategoryResult = await categories.add(newCategory);
+            addCategoryResult = await todos.add(newCategory);
             expect(addCategoryResult, isNotNull);
           });
         test("remove item from collection", () async {
-            await categories.removeFromId(addCategoryResult.id);
+            await todos.removeFromId(addCategoryResult.id);
           });
+      });
       });
 
       group("sockets", () {
         test("subscribe to channel and broadcast", () async {
           minioClient.broadcast({"message": "Hello", "from": "admin"});
-          await Future.delayed(Duration(seconds: 10));
+          await Future.delayed(Duration(seconds: 60));
         });
       });
   });
---
+++ tests/todo.dart
@@ -0,0 +1,35 @@
+
+import 'package:minio_client_dart/convertable_Item.dart';
+
+import 'category.dart';
+
+class Todo extends ConvertableItem{
+  final String id;
+  final String title;
+  final String description;
+  final DateTime creationDate;
+  final bool isDone;
+  final Category category;
+
+  Todo(Map<String, dynamic> json):
+    id = json["_id"],
+    title = json["title"],
+    description = json["description"],
+    creationDate = json["creationDate"] != null ? DateTime.tryParse(json["creationDate"]) : DateTime.fromMicrosecondsSinceEpoch(0),
+    isDone = json["isDone"],
+    category = Category(json);
+  
+  Todo.Init(this.id, this.title, this.description, this.creationDate, this.isDone, this.category);
+
+  Map<String, dynamic> toJson() => {
+    '_id' : id,
+    'title': title,
+    'description': description,
+    'creationDate': creationDate.toIso8601String(),
+    'isDone': isDone,
+    'catgory': category.toJson()
+  };
+
+  @override
+  bool isValid() => id.isNotEmpty;
+}
